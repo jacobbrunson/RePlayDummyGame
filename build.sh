2fc74fce0614f3e51ffdda960a3bf1ab2494bd13
Jacob Brunson <jacob@brunson.me>
2018-09-20 15:55:54 -0500

    forgot to make directory

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 msbuild DummyGame /p:Configuration=Release
 paket pack DummyGame
+mkdir ../../../../RePlay/Assets/DummyGame/
 cp -R DummyGame/Content/bin/Android/* ../../../../RePlay/Assets/DummyGame/
